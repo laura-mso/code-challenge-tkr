f39dcfbe4e69e5e096f0819f7cd96affb4d89a4e
laura-mso <laura.socha@gmx.net>
2019-06-27 19:19:21 +0200

    Updated design

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -4,7 +4,7 @@ export default function Heading() {
   return (
     <div className='flex-section'>
       <div>
-        <h1 className='heading1'>Browser Site</h1>
+        <h1 className='heading1'>Browse Site</h1>
       </div>
       <div>
         <div className='nostyle heading2'>Select a Tab</div>
